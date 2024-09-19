829e05760525fb51bb0b2855a375ffb58616940f
bpm02 <bpm1201@gmail.com>
2024-09-19 14:06:13 +0900

    APIのデータの画面出力に成功

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,20 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { fetchPageData } from "../app/lib/services/userApi";
-// import { Link } from "next/link";
+import { fetchPageData } from "./lib/services/userApi";
+import Link from "next/link";
 import Image from "next/image";
 
 
 export default function Home() {
 
-  const [pageData, setPageData] = useState({});
+  const [pageData, setPageData] = useState(null);
 
   useEffect(() => {
     fetchPageData().then((data) => {
-      setPageData(data)
-      console.log(data);
+      const array = Object.entries(data.contents);
+      setPageData(array)
+      console.log(array);
     });
   }, []);
 
@@ -22,11 +23,11 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1>Hello world!</h1>
         <ul>
-          <li><a href=""></a></li>
-          <li><a href=""></a></li>
-          <li><a href=""></a></li>
+          {pageData ? (pageData.map((item, index) => (
+            <li key={index}><Link href={`post/${item[1]['id']}`}> {item[1]['title']}</Link></li>
+          ))) : (<li>Loding...</li>)}
         </ul>
       </div>
-    </main>
+    </main >
   );
 }
