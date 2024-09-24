ff5136244dbd659e89c49fb4ccd994a20dfa7d78
bpm02 <bpm1201@gmail.com>
2024-09-24 16:58:17 +0900

    アイキャッチ画像を設定

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -22,10 +22,29 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1>Hello world!</h1>
-        <ul>
+        <div className="profile grid gap-5">
+          <div className="profile__text">
+            <dl>
+              <dt>原田 大介</dt>
+              <dd>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</dd>
+            </dl>
+          </div>
+          <div className="profile__image">
+          </div>
+        </div>
+        <h1>ポートフォリオ</h1>
+        <ul className="grid grid-cols-3 gap-3">
           {pageData ? (pageData.map((item, index) => (
-            <li key={index}><Link href={`post/${item[1]['id']}`}> {item[1]['title']}</Link></li>
+            <li key={index}>
+              <Link href={`post/${item[1]['id']}`} className="flex items-center justify-center bg-black text-white aspect-square p-4">
+                <Image
+                  src={item[1]['eyecatch']['url']}
+                  width={item[1]['eyecatch']['width']}
+                  height={item[1]['eyecatch']['height']}
+                  alt="アイキャッチ画像"
+                />
+                {item[1]['title']}</Link>
+            </li>
           ))) : (<li>Loding...</li>)}
         </ul>
       </div>
