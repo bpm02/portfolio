dbaf7df3a8bbf2dcf221566bdb512cbd6cd8a048
bpm02 <bpm1201@gmail.com>
2025-01-13 10:22:55 +0900

    next.configで環境変数を設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        // .env.localやVercelから取得
+        API_URL: process.env.API_URL,
+        MICRO_CMS_API_KEY: process.env.MICRO_CMS_API_KEY,
+        BASIC_AUTH_NAME: process.env.BASIC_AUTH_NAME,
+        BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD
+    },
     images: {
         remotePatterns: [
             {
